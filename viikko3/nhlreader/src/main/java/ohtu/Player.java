e2d2c5b27906c506764e74387e78de8a2d3679d0
chaurea <chaureus@gmail.com>
2020-11-15 06:06:16 +0200

    tehtävät 1-2

--- viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,12 @@
 
 package ohtu;
 
-public class Player {
+public class Player implements Comparable<Player> {
     private String name;
+    private String nationality;
+    private String team;
+    private int assists;
+    private int goals;
 
     public void setName(String name) {
         this.name = name;
@@ -12,9 +16,46 @@ public class Player {
         return name;
     }
 
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    @Override
+    public int compareTo(Player o) {
+        return o.assists + o.goals - this.assists - this.goals;
+    }
+
     @Override
     public String toString() {
-        return name;
+        return name + "\t\t" + team + "\t" + goals + " + " + assists + " = " + (goals + assists);
     }
-      
+
 }
