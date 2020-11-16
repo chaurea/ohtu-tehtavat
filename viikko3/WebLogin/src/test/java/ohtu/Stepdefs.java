2df1156c64eea07a164bfa60baaf572904d75223
chaurea <chaureus@gmail.com>
2020-11-16 03:55:50 +0200

    tehtävä 9

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -37,6 +37,11 @@ public class Stepdefs {
     public void correctUsernameAndIncorrectPasswordAreGiven(String username, String password) {
         logInWith(username, password);
     }    
+
+        @When("incorrect username {string} and password {string} are given")
+    public void incorrectUsernameAndCorrectPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }    
     
     @Then("user is not logged in and error message is given")
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
